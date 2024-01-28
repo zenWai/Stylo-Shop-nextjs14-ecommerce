95d35e59ff7ea336c3e54f446eb0fca20fc6d569
Presa <ivopresa@gmail.com>
2024-01-28 21:36:50 +0100

    "Update tile.tsx styles."
    - Changed the tile's border color to gray-400.
    - Changed the tile's background color to customBeige.
    - Changed the tile's hover border color to blushPink.
    - Changed the image object-fit style to object-fill.

--- components/grid/tile.tsx
+++ components/grid/tile.tsx
@@ -20,7 +20,7 @@ export function GridTileImage({
   return (
     <div
       className={clsx(
-        'group flex h-full w-full items-center justify-center overflow-hidden rounded-lg border bg-white hover:border-blue-600',
+        'group flex h-full w-full items-center justify-center overflow-hidden rounded-lg border border-gray-400 bg-customBeige hover:border-blushPink',
         {
           relative: label,
           'border-2 border-blue-600': active,
@@ -31,7 +31,7 @@ export function GridTileImage({
       {props.src ? (
         // eslint-disable-next-line jsx-a11y/alt-text -- `alt` is inherited from `props`, which is being enforced with TypeScript
         <Image
-          className={clsx('relative h-full w-full object-contain', {
+          className={clsx('relative h-full w-full object-fill', {
             'transition duration-300 ease-in-out group-hover:scale-105': isInteractive
           })}
           {...props}
