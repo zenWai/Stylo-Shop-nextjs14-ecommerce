356971396ec9351aea50171546947b7a24913c70
Presa <ivopresa@gmail.com>
2024-01-28 21:33:14 +0100

    "Add SkeletonSearchInput to Navbar"
    - Created SkeletonSearchInput
    - Added the SkeletonSearchInput FallBack to the Navbar component in the mobile-menu.tsx and index.tsx files
    
    Changes:
    - Updated the styles of the search input in the search.tsx file
    - Sticky Navbar
    - Styles

--- components/layout/navbar/index.tsx
+++ components/layout/navbar/index.tsx
@@ -1,3 +1,4 @@
+import SkeletonSearchInput from '@/components/skeletons/search-input';
 import Cart from 'components/cart';
 import OpenCart from 'components/cart/open-cart';
 import LogoSquare from 'components/logo-square';
@@ -13,46 +14,51 @@ export default async function Navbar() {
   const menu = await getMenu('next-js-frontend-header-menu');
 
   return (
-    <nav className="relative flex items-center justify-between p-4 lg:px-6">
-      <div className="block flex-none md:hidden">
-        <Suspense>
-          <MobileMenu menu={menu} />
-        </Suspense>
-      </div>
-      <div className="flex w-full items-center">
-        <div className="flex w-full md:w-1/3">
-          <Link href="/" className="mr-2 flex w-full items-center justify-center md:w-auto lg:mr-6">
-            <LogoSquare />
-            <div className="ml-2 flex-none text-sm font-medium uppercase md:hidden lg:block">
-              {SITE_NAME}
-            </div>
-          </Link>
-          {menu.length ? (
-            <ul className="hidden gap-6 text-sm md:flex md:items-center">
-              {menu.map((item: Menu) => (
-                <li key={item.title}>
-                  <Link
-                    href={item.path}
-                    className="text-neutral-500 underline-offset-4 hover:text-black hover:underline"
-                  >
-                    {item.title}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          ) : null}
-        </div>
-        <div className="hidden justify-center md:flex md:w-1/3">
+    <header className="sticky top-0 z-50">
+      <nav className="relative flex items-center justify-between bg-customBeige p-4 shadow-md lg:px-6">
+        <div className="block flex-none md:hidden">
           <Suspense>
-            <Search />
+            <MobileMenu menu={menu} />
           </Suspense>
         </div>
-        <div className="flex justify-end md:w-1/3">
-          <Suspense fallback={<OpenCart />}>
-            <Cart />
-          </Suspense>
+        <div className="flex w-full items-center">
+          <div className="flex w-full md:w-1/3">
+            <Link
+              href="/"
+              className="mr-2 flex w-full items-center justify-center md:w-auto lg:mr-6"
+            >
+              <LogoSquare />
+              <div className="ml-2 flex-none font-greatVibes text-2xl font-bold text-black md:hidden lg:block">
+                {SITE_NAME}
+              </div>
+            </Link>
+            {menu.length ? (
+              <ul className="hidden gap-6 text-sm md:flex md:items-center">
+                {menu.map((item: Menu) => (
+                  <li key={item.title}>
+                    <Link
+                      href={item.path}
+                      className="text-black underline-offset-4 hover:text-black hover:underline"
+                    >
+                      {item.title}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            ) : null}
+          </div>
+          <div className="hidden justify-center md:flex md:w-1/3">
+            <Suspense fallback={<SkeletonSearchInput />}>
+              <Search />
+            </Suspense>
+          </div>
+          <div className="flex justify-end md:w-1/3">
+            <Suspense fallback={<OpenCart />}>
+              <Cart />
+            </Suspense>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </header>
   );
 }
