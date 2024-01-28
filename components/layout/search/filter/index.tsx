8d5234ae84c6130f9d62fffeb302493b9234dd0b
Presa <ivopresa@gmail.com>
2024-01-28 03:23:56 +0100

    Remove dark theme

--- components/layout/search/filter/index.tsx
+++ components/layout/search/filter/index.tsx
@@ -20,7 +20,7 @@ export default function FilterList({ list, title }: { list: ListItem[]; title?: 
     <>
       <nav>
         {title ? (
-          <h3 className="hidden text-xs text-neutral-500 dark:text-neutral-400 md:block">
+          <h3 className="hidden text-xs text-neutral-500 md:block">
             {title}
           </h3>
         ) : null}
--- components/product/product-description.tsx
+++ components/product/product-description.tsx
@@ -7,7 +7,7 @@ import { VariantSelector } from './variant-selector';
 export function ProductDescription({ product }: { product: Product }) {
   return (
     <>
-      <div className="mb-6 flex flex-col border-b pb-6 dark:border-neutral-700">
+      <div className="mb-6 flex flex-col border-b pb-6">
         <h1 className="mb-2 text-5xl font-medium">{product.title}</h1>
         <div className="mr-auto w-auto rounded-full bg-blue-600 p-2 text-sm text-white">
           <Price
@@ -20,7 +20,7 @@ export function ProductDescription({ product }: { product: Product }) {
 
       {product.descriptionHtml ? (
         <Prose
-          className="mb-6 text-sm leading-tight dark:text-white/[60%]"
+          className="mb-6 text-sm leading-tight"
           html={product.descriptionHtml}
         />
       ) : null}
