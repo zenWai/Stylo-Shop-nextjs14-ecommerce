b3f36bd0ba0ead66957beefb74052780fddc4942
Presa <ivopresa@gmail.com>
2024-01-28 03:08:01 +0100

    GeistSans from 'geist/font/sans' instead of 'geist/font'

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import Navbar from 'components/layout/navbar';
-import { GeistSans } from 'geist/font';
+import { GeistSans } from 'geist/font/sans';
 import { ensureStartsWith } from 'lib/utils';
 import { ReactNode, Suspense } from 'react';
 import './globals.css';
@@ -33,7 +33,7 @@ export const metadata = {
 
 export default async function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en" className={GeistSans.variable}>
+    <html lang="en" className={GeistSans.className}>
       <body className="bg-neutral-50 text-black selection:bg-teal-300 dark:bg-neutral-900 dark:text-white dark:selection:bg-pink-500 dark:selection:text-white">
         <Navbar />
         <Suspense>
