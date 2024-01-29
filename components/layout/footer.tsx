3c8a72c812bd7d67d4c9268b68142ffa1193255f
Presa <ivopresa@gmail.com>
2024-01-29 12:34:32 +0100

    Update footer and logo for page & metadata
    - Footer background color "bg-customBeige".
    - Logo background color "bg-customBeige".
    - New logo @ LogoIcon component
    - Adjusted the size and shape of the logo component based on the "size" prop.

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -13,7 +13,7 @@ export default async function Footer() {
   const copyrightName = COMPANY_NAME || SITE_NAME || '';
 
   return (
-    <footer className="text-sm text-neutral-500 ">
+    <footer className="bg-customBeige text-sm text-black">
       <div className="mx-auto flex w-full max-w-7xl flex-col gap-6 border-t border-neutral-200 px-6 py-12 text-sm md:flex-row md:gap-12 md:px-4 min-[1320px]:px-0">
         <div>
           <Link className="flex items-center gap-2 text-black md:pt-1" href="/">
@@ -26,7 +26,7 @@ export default async function Footer() {
         <FooterMenu menu={menu} />
       </div>
       <div className="border-t border-neutral-200 py-6 text-sm">
-        <div className="mx-auto flex w-full max-w-7xl flex-col items-center gap-1 px-4 md:flex-row md:gap-0 md:px-4 min-[1320px]:px-0">
+        <div className="mx-auto flex w-full max-w-7xl flex-col items-center gap-1 px-4 text-black md:flex-row md:gap-0 md:px-4 min-[1320px]:px-0">
           <p>
             &copy; {copyrightDate} {copyrightName}
             {copyrightName.length && !copyrightName.endsWith('.') ? '.' : ''} All rights reserved.
--- components/logo-square.tsx
+++ components/logo-square.tsx
@@ -1,11 +1,11 @@
 import clsx from 'clsx';
 import LogoIcon from './icons/logo';
 
-export default function LogoSquare({ size }: { size?: 'sm' | undefined }) {
+export default function LogoSquare({ size }: { size?: 'sm' }) {
   return (
     <div
       className={clsx(
-        'flex flex-none items-center justify-center border border-neutral-200 bg-white',
+        'flex flex-none items-center justify-center border border-neutral-200 bg-customBeige',
         {
           'h-[40px] w-[40px] rounded-xl': !size,
           'h-[30px] w-[30px] rounded-lg': size === 'sm'
@@ -14,8 +14,8 @@ export default function LogoSquare({ size }: { size?: 'sm' | undefined }) {
     >
       <LogoIcon
         className={clsx({
-          'h-[16px] w-[16px]': !size,
-          'h-[10px] w-[10px]': size === 'sm'
+          'h-[40px] w-[40px] rounded-xl': !size,
+          'h-[30px] w-[30px] rounded-lg': size === 'sm'
         })}
       />
     </div>
