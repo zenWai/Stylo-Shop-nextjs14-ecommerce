3fb04add30de72f854163cf484689733652a7519
Presa <ivopresa@gmail.com>
2024-01-30 21:32:08 +0100

    chore(eslint): update .eslintrc.js
    - Update extends with @vercel/style-guide/eslint/_base, @vercel/style-guide/eslint/browser, @vercel/style-guide/eslint/react, @vercel/style-guide/eslint/next, @vercel/style-guide/eslint/typescript, @vercel/style-guide/eslint/node, next, prettier, next/core-web-vitals
    - Update parserOptions with ecmaVersion, sourceType, project
    
    chore(package.json): update dependencies
    
    chore(prettier.config.js): update config
    
    - Update config with styleguide, plugins

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,22 @@
+const { ECMA_VERSION } = require('@vercel/style-guide/eslint/constants');
+
 module.exports = {
-  extends: ['next', 'prettier'],
-  plugins: ['unicorn'],
+  extends: [
+    require.resolve('@vercel/style-guide/eslint/_base'),
+    require.resolve('@vercel/style-guide/eslint/browser'),
+    require.resolve('@vercel/style-guide/eslint/react'),
+    require.resolve('@vercel/style-guide/eslint/next'),
+    require.resolve('@vercel/style-guide/eslint/typescript'),
+    require.resolve('@vercel/style-guide/eslint/node'),
+    'next',
+    'prettier',
+    'next/core-web-vitals',
+  ],
+  parserOptions: {
+    ecmaVersion: ECMA_VERSION,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
   rules: {
     'no-unused-vars': [
       'error',
@@ -8,16 +24,16 @@ module.exports = {
         args: 'after-used',
         caughtErrors: 'none',
         ignoreRestSiblings: true,
-        vars: 'all'
-      }
+        vars: 'all',
+      },
     ],
     'prefer-const': 'error',
     'react-hooks/exhaustive-deps': 'error',
     'unicorn/filename-case': [
       'error',
       {
-        case: 'kebabCase'
-      }
-    ]
-  }
+        case: 'kebabCase',
+      },
+    ],
+  },
 };
