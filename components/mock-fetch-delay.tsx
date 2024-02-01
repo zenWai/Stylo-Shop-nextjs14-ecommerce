b2bd2267277b982b981f2ebc56c9daa0e3d1a22d
Presa <ivopresa@gmail.com>
2024-02-01 03:06:15 +0100

    "update mockFetchDelay function types"
    - Update the signature of the `mockFetchDelay` function to include type annotations for the `fetchFunction` parameter and the return type.
    - Change the function to use arrow function syntax for better readability.

--- components/mock-fetch-delay.tsx
+++ components/mock-fetch-delay.tsx
@@ -2,9 +2,12 @@
  * Feeds a function that will return a promise with a delay
  * good to test skeletons and loading states
  */
-// @ts-ignore
-export function mockFetchDelay(fetchFunction, delay = 3000) {
-  return new Promise((resolve, reject) => {
+
+export function mockFetchDelay<T>(
+  fetchFunction: () => Promise<T>,
+  delay = 3000,
+): Promise<T> {
+  return new Promise<T>((resolve, reject) => {
     setTimeout(() => {
       fetchFunction().then(resolve).catch(reject);
     }, delay);
