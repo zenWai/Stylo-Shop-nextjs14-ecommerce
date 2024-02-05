2fcf395297e991eac76268530b0e293f8358ccbc
Presa <ivopresa@gmail.com>
2024-02-05 20:51:13 +0100

    feat(product): add product display price component
    - add product display price component to calculate the display price for a product based on the provided search parameters and/or selected options which is practically the same
    - add product display price component to calculate the display price for a product based on the provided search parameters and return the minimum variant price if all variants have the same price or if the search parameters do not match valid product options
    - add product display price component to calculate the display price for a product based on the provided search parameters and find the matching variant based on the search parameters and return its price if it exists, otherwise return the minimum variant price

--- components/product/product-description.tsx
+++ components/product/product-description.tsx
@@ -1,27 +1,51 @@
 import { Suspense } from 'react';
+import { productDisplayPrice } from '@/components/product/product-display-price';
+import { getCachecombinations } from '@/components/product/product-cached-functions';
 import { AddToCart } from 'components/cart/add-to-cart';
 import Price from 'components/price';
 import Prose from 'components/prose';
 import type { Product } from 'lib/shopify/types';
 import { VariantSelector } from './variant-selector';
 
-export function ProductDescription({ product }: { product: Product }) {
+export async function ProductDescription({
+  product,
+  searchParamsProductPage,
+}: {
+  product: Product;
+  searchParamsProductPage: URLSearchParams;
+}) {
+  const hasNoOptionsOrJustOneOption =
+    !product.options.length ||
+    (product.options.length === 1 && product.options[0]?.values.length === 1);
+
+  const displayPrice = await productDisplayPrice(
+    product,
+    searchParamsProductPage,
+  );
+
+  const cachedCombinations = hasNoOptionsOrJustOneOption
+    ? []
+    : await getCachecombinations(product.variants);
+
   return (
     <>
       <div className="mb-6 flex flex-col border-b pb-6">
         <h1 className="mb-2 text-5xl font-medium">{product.title}</h1>
         <div className="mr-auto w-auto rounded-full bg-coralPink p-2 text-sm text-white">
           <Price
-            amount={product.priceRange.maxVariantPrice.amount}
-            currencyCode={product.priceRange.maxVariantPrice.currencyCode}
+            amount={displayPrice.amount}
+            currencyCode={displayPrice.currencyCode}
           />
         </div>
       </div>
       <Suspense>
-        <VariantSelector
-          options={product.options}
-          variants={product.variants}
-        />
+        {!hasNoOptionsOrJustOneOption ? (
+          <VariantSelector
+            cachedCombinations={cachedCombinations}
+            options={product.options}
+            variants={product.variants}
+          />
+        ) : null}
       </Suspense>
       {product.descriptionHtml ? (
         <Prose
